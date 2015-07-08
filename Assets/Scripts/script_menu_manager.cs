1930d964c82945c4159a652822488b2c833578fa
lcy <cavegamestudio@gmail.com>
2015-07-09 05:02:32 +0800

    Basic News Display Completed

--- Assets/Scripts/script_menu_manager.cs
+++ Assets/Scripts/script_menu_manager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Threading.Tasks;
 using Parse;
 using System.Collections.Generic;
+using System;
 
 public class script_menu_manager : MonoBehaviour {
 
@@ -35,7 +36,7 @@ public class script_menu_manager : MonoBehaviour {
 	public GameObject grp_continue;
 	public GameObject grp_load;
 	public GameObject grp_options;
-	Dictionary<string, GameObject> GROUP = new Dictionary<string, GameObject>();
+	public Dictionary<string, GameObject> GROUP = new Dictionary<string, GameObject>();
 
 	//Panel Title
 	public Text txt_main_title;
@@ -46,17 +47,15 @@ public class script_menu_manager : MonoBehaviour {
 	public bool news_forceupdate;
 
 	//News Template Reference
-	public GameObject template_item_group;
-	public GameObject template_item_title;
-	public GameObject template_item_post_detail;
-	public GameObject template_item_post_content;
+	
+	public GameObject[] grps = new GameObject[10];
+	public Text[] titles = new Text[10];
+	public Text[] details = new Text[10];
+	public Text[] contents = new Text[10];
 
 	// News Text Reference
 	public IEnumerable<ParseObject> news_from_parse;
-
-	// to be deleted
-	public Text txt_main_news;
-	public string news;
+	public Text txt_news_loading;
 
 	public string display_currentGroup;
 	public string display_prevGroup;
@@ -80,140 +79,189 @@ public class script_menu_manager : MonoBehaviour {
 		display_currentGroup = CONST_ID_NEWS;
 		display_prevGroup = display_currentGroup;
 		updateNews();
+
 	}
 
 	void Update () {
 		switch (display_currentGroup) {
 
 			default:
-			break;
+				break;
 			case CONST_ID_REPORT:
-			break;
+				break;
 			case CONST_ID_ACHIEVEMENT:
-			break;
+				break;
 			case CONST_ID_NEWGAME:
-			break;
+				break;
 			case CONST_ID_CONTINUE:
-			break;
+				break;
 			case CONST_ID_LOAD:
-			break;
+				break;
 			case CONST_ID_OPTIONS:
-			break;
+				break;
 			case CONST_ID_NEWS:
-			if(!btn_main_refresh.GetComponent<Button> ().enabled) {
-				btn_main_refresh.GetComponent<Button> ().enabled = true;
-			}
 
-
-			if (news_updated == false) {
-				if (news_loading == true) {
-					txt_main_news.text = "Loading...";
-				}				else {
-					//TODO 
-					txt_main_news.text = news;
-					news_updated = true;
+				if(!btn_main_refresh.GetComponent<Button> ().enabled) {
+					btn_main_refresh.GetComponent<Button> ().enabled = true;
 				}
-			}
+
+				if (news_updated == false) {
+					if (news_loading == true) {
+						txt_news_loading.enabled = true;
+					} else {
+						txt_news_loading.enabled = false;
+						
+						string news_title;
+						string news_author;
+						string news_time;
+						string news_content;
+					
+						int counter = 1;
+
+						foreach (var news_item in news_from_parse){
+					
+							news_title 		= news_item.Get<string>("title");
+							news_author 	= news_item.Get<string>("author");
+							news_time		= news_item.CreatedAt.ToString();
+							news_content 	= news_item.Get<string>("content");
+
+							titles[counter-1].text = news_title;
+							details[counter-1].text = "Published By: " + news_author + "\nPublished On: " + news_time;
+							contents[counter-1].text = news_content + "\n\n";
+							counter ++;
+						}
+
+					for(int i = 0; i < 10; i ++){
+						if(titles[i].text.Equals ("")){
+							grps[i].SetActive (false);
+						} else {
+							grps[i].SetActive (true);
+						}
+					}
 
 
-			break;
+						Debug.Log("news updated.\n");
+						news_updated = true;
+					}
+				}
+				break;
 		}
 	}
 
 	public void onClick_main_new() {
-		Debug.Log("Button Pressed : Main - New Game");
+		
+		Debug.Log("Button Pressed : Main - New Game\n");
 		txt_main_title.text = "New Game";
 		display_currentGroup = CONST_ID_NEWGAME;
 		toggleGroups();
+		
 	}
 
 	public void onClick_main_continue() {
-		Debug.Log("Button Pressed : Main - Continue");
+		
+		Debug.Log("Button Pressed : Main - Continue\n");
 		txt_main_title.text = "Continue";
 		display_currentGroup = CONST_ID_CONTINUE;
 		toggleGroups();
+		
 	}
 
 	public void onClick_main_load() {
-		Debug.Log("Button Pressed : Main - Load");
+		
+		Debug.Log("Button Pressed : Main - Load\n");
 		txt_main_title.text = "Load Game";
 		display_currentGroup = CONST_ID_LOAD;
 		toggleGroups();
+		
 	}
 
 	public void onClick_main_options() {
-		Debug.Log("Button Pressed : Main - Options");
+		
+		Debug.Log("Button Pressed : Main - Options\n");
 		txt_main_title.text = "Options";
 		display_currentGroup = CONST_ID_OPTIONS;
 		toggleGroups();
+		
 	}
 
 	public void onClick_main_news() {
-		Debug.Log("Button Pressed : Main - News");
+		
+		Debug.Log("Button Pressed : Main - News\n");
 		txt_main_title.text = "News";
 		display_currentGroup = CONST_ID_NEWS;
 		toggleGroups();
 		updateNews();
+		
 	}
 
 	public void onClick_main_achievement() {
-		Debug.Log("Button Pressed : Main - Achievement");
+		
+		Debug.Log("Button Pressed : Main - Achievement\n");
 		txt_main_title.text = "Achievements";
 		display_currentGroup = CONST_ID_ACHIEVEMENT;
 		toggleGroups();
+		
 	}
 
 	public void onClick_main_report() {
-		Debug.Log("Button Pressed : Main - Report");
+		
+		Debug.Log("Button Pressed : Main - Report\n");
 		txt_main_title.text = "Report Bugs/Issues";
 		display_currentGroup = CONST_ID_REPORT;
 		toggleGroups();
+		
 	}
 
 	public void onClick_main_refresh() {
-		Debug.Log("Button Pressed : Main - Refresh");
+		
+		Debug.Log("Button Pressed : Main - Refresh\n");
 		btn_main_refresh.GetComponent<Button>().enabled = false;
 		news_forceupdate = true;
 		updateNews();
 		news_forceupdate = false;
 		btn_main_refresh.GetComponent<Button>().enabled = true;
+		
 	}
 
 	public void onClick_main_quit() {
-		Debug.Log("Button Pressed : Main - Quit");
+		
+		Debug.Log("Button Pressed : Main - Quit\n");
+		
 		Application.Quit();
 	}
 
 	void toggleGroups() {
+		
 		GROUP[display_prevGroup].SetActive(false);
 		GROUP[display_currentGroup].SetActive(true);
 		display_prevGroup = display_currentGroup;
+		
 	}
 
 	void updateNews() {
-		if (!news.Equals ("") && !news_forceupdate){
+		if (news_from_parse!=null && !news_forceupdate){
+			Debug.Log("News is already loaded, no force update\n");
 			return;
 		}
+
+		for(int i = 0; i < 10; i ++){
+			grps[i].SetActive (false);
+			titles[i].text = "";
+		}
 			
-		Debug.Log("Downloading news data from parse...");
+		Debug.Log("downloading news...\n");
 		news_loading = true;
 		news_updated = false;
-		
+
 		var query = ParseObject.GetQuery("News")
-			.OrderByDescending("updatedAt")
-				.Limit (10);
+			.OrderByDescending("createdAt")
+			.Limit (10);
 		query.FindAsync().ContinueWith(t => {
-			IEnumerable<ParseObject> articles = t.Result;
-			news = "";
-			foreach (var article in articles){
-				news += article.Get<string>("title") + "\n<hr>";
-				news += article.Get<string>("content") + "\n<hr>";
-			}
-			Debug.Log("News updated");
+			news_from_parse = t.Result;
 			news_loading = false;
+			Debug.Log ("news downloaded.\n");
 		});
 		news_forceupdate = false;
-		
 	}
 	
 }
--- Assets/Scripts/script_system_debug_manager.cs
+++ Assets/Scripts/script_system_debug_manager.cs
@@ -1,11 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class script_system_debug_manager : MonoBehaviour {
 
-	public string[] callStack;
+//	public int stackCount;
+	public List<string> callStack;
 
-	public void func_call(string function_name){
-		Debug.Log("STACK");
+	void Start(){
+
 	}
+
+	public void func_call(string obj_name, string function_name){
+		string debugMsg = "";
+		foreach(string parent in callStack){
+			debugMsg += ">>" + parent;
+		}
+		callStack.Add (obj_name + "." + function_name);
+		Debug.Log("STACK::" + debugMsg + ">>" + obj_name + "." + function_name + "\n");
+	}
+
+	public void func_exit(string obj_name, string function_name){
+		callStack.Remove (obj_name + "." + function_name);
+		string debugMsg = "";
+		foreach(string parent in callStack){
+			debugMsg += parent + "=>";
+		}
+		Debug.Log("STACK::" + debugMsg + "<<" + obj_name + "." + function_name + "\n");
+	}
+
 }
